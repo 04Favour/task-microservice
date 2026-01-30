94ff3d8f283f5304871eac939d54431419b0ac0a
Favour Mfon <favourmichael004@gmail.com>
2026-01-30 15:33:57 +0100

    Push to production

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
     .build();
 
   const documentFactory = ()=> SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, documentFactory)
+  SwaggerModule.setup('app', app, documentFactory)
   await app.listen(process.env.PORT ?? 3009);
 }
 bootstrap();

311baba236ed5c8dd41ce9752a550c6911974b21
Favour Mfon <favourmichael004@gmail.com>
2026-02-02 15:37:58 +0100

    Add cors and global prefix

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1')
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({
     forbidNonWhitelisted: true,
     whitelist: true,
